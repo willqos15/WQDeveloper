74638633036c4dacde0e11005b70152d1c5ca073
William Queiroz de Oliveira Souza <willqos15@gmail.com>
2025-12-28 20:24:45 -0300

    fix bug mobile

--- src/components/cardinfo.jsx
+++ src/components/cardinfo.jsx
@@ -8,44 +8,49 @@ export default function CardInfo() {
 
     return (<>
 
-<h2 id= "sobre" className={styles.haltura}></h2>
 
-        <div 
-        
-        className={styles.rollimg}>
-            
-            <div  className={styles.backimg}>
-                <img src={imagem}  />
-            </div>
 
-            
-            <div className={styles.degrade} >
 
+   <section
                 
-
-                <motion.section
-                
-                // viewport={{ once: true }}
-                style={{originY: 0}}
-                initial = {{scaleY: 0, opacity: 0}}
-        whileInView={{scaleY:1, opacity:1}}
+        //          viewport={{ once: true }}
+        //         style={{originY: 0}}
+        //         initial = {{scaleY: 0, opacity: 0}}
+        // whileInView={{scaleY:1, opacity:1}}
   
-        transition={{duration: 0.6, ease: "easeIn"}}
-         className={styles.bloco}>
-                    <div s>
-                    <h2  className={styles.hsobre}>Sobre mim: </h2>
+        // transition={{duration: 0.6, ease: "easeIn"}}
+         className={styles.bloco}  >
+            <div id= "sobre" className={styles.ancora}>
+                <div className={styles.barratop } >
+            <h2  className={styles.hsobre}>Sobre mim: </h2>
+            </div>
+                    <div className={styles.cardsobre}>
+                    
+                    <div className={styles.textoi}>
                     <p>Sou ex-professor de robótica educacional e formado em Engenharia da Computação. Atualmente em transição de carreira para o desenvolvimento web.</p>
                      <p>Busco meu primeiro emprego em vagas remotas como <strong>dev. júnior, estagiário ou trainee.</strong> Estou aberto ao aprendizado, sejam em novas linguagens, tecnologias, frontend ou backend.</p>
                      </div>
-
+                     
+                    <div>
                     <img className={styles.foto4x4} src={foto}></img>
+                    </div>
+                    </div>
+                    </div>
 
-                </motion.section>
-                
-            </div>
-            
-        </div>
+                </section>
+
+
+
+
         
+
+          
+        
+            
+       
+
+        
+               
 
 
     </>)
