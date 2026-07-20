84f85f02b174c0e08487e5cbba96c71338d4c916
William Queiroz de Oliveira Souza <willqos15@gmail.com>
2026-07-20 18:13:26 -0300

    fix import themecontext app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import Skills from './features/Skills'
 import AboutMe from './features/AboutMe'
 import Recommendation from './features/Recommendation'
 import { IoIosArrowUp } from "react-icons/io";
-import { ThemeProvider } from './contexts/Themecontext'
 import Projects from './features/Projects'
 import Certifications from './features/Certifications'
+import { ThemeProvider } from './contexts/ThemeContext'
+
 
 function App() {
  
--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -1,5 +1,6 @@
-import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { useContext } from "react";
+
 
 
 export function useTheme() {
